83c7e5fab09cdd83694c82ff75b257e3e3e4570d
nathalieforsythe <nforsythe22@penguinhall.org>
2022-03-24 09:30:23 -0400

    Started homework

---
+++ refresher.js
@@ -0,0 +1,39 @@
+let message = 'How are you today?';
+let greeting = 'Hello';
+let name = 'Sue';
+let age = 13;
+let iAmWearingABelt = false;
+let iAmWearingGlasses = false;
+
+// console.log(message);
+// console.log(greeting + ' ' + name + '!'); // name is deprecated?
+// console.log('You are ' + age);
+// console.log('In ten years you will be ' + (age + 10));
+
+// Part 2
+// if (name == 'Bob'){
+//     console.log('I knew it was you, ' + name + '!')
+// } else {
+//     console.log('Hey ' + name + ', you\'re not Bob!')
+// }
+
+// if (iAmWearingABelt == true && iAmWearingGlasses == true){
+//     console.log('Probably not Mr. Gorton')
+// } else if (iAmWearingABelt == true && iAmWearingGlasses == false){
+//     console.log('Probably is Mr. Gorton')
+// } else{
+//     console.log('Probably just some dude')
+// }
+
+// Part 3
+for (let i = 0; i < 6; i++) {
+    console.log('comp sci is awesome');
+}
+
+for (let i = 0; i < 11; i++) {
+    console.log(i);
+}
+
+function tellMeMyAge () {
+    
+}
