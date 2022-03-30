45eccba1a30b7c6e8706973c3de9eef5f058f353
nathalieforsythe <nforsythe22@penguinhall.org>
2022-03-30 12:10:54 -0400

    got fizzbuzz to work

--- fizz-buzz/fizz-buzz.js
+++ fizz-buzz/fizz-buzz.js
@@ -1,11 +1,20 @@
-for (let i = 0; i < 1000; i++) {
-    console.log(i + 1);
+let fizz = 0; 
+let buzz = 0; 
+let fizzBuzz = 0;
 
-    if ((i % 3) = 0) {
-        console.log(i + 'fizz');
-    } else if ((i % 5) = 0) {
-        console.log(i + 'buzz');
-    } else if (((i % 3) = 0) && ((i % 5) = 0)) {
-        console.log(i + 'fizz buzz');
+for (let i = 1; i < 1001; i++) {
+    if (((i % 3) == 0) && ((i % 5) == 0)) {
+        console.log(i + ' fizz buzz');
+        fizzBuzz++;
+    } else if ((i % 3) == 0) {
+        console.log(i + ' fizz');
+        fizz++;
+    } else if ((i % 5) == 0) {
+        console.log(i + ' buzz');
+        buzz++;
+    } else {
+        console.log(i);
     }
 }
+
+console.log('There were ' + fizz + ' fizz\'s, ' + buzz + ' buzz\'s, and ' + fizzBuzz + ' fizz buzz\'s out of 1000');
