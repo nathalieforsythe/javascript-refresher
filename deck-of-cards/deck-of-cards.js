ff5c5a9c315b48ded338174fbb63a14403d673c2
nathalieforsythe <nforsythe22@penguinhall.org>
2022-03-31 18:22:34 -0400

    switched for in to for of

--- deck-of-cards/deck-of-cards.js
+++ deck-of-cards/deck-of-cards.js
@@ -1,18 +1,18 @@
 // Array
 let suits = ['Hearts', 'Spades', 'Diamonds', 'Clubs'];
 
-for (let suit in suits) {
+for (let suit of suits) {
     countCards(suit);
 }
 
 function countCards (suit) {
-    console.log('Ace of ' + suits[suit]);
+    console.log('Ace of ' + suit);
     for (let i = 2; i <= 10; i++) {
-        console.log(i + ' of ' + suits[suit]);
+        console.log(i + ' of ' + suit);
     }
-    console.log('Jack of ' + suits[suit]);
-    console.log('Queen of ' + suits[suit]);
-    console.log('King of ' + suits[suit]);
+    console.log('Jack of ' + suit);
+    console.log('Queen of ' + suit);
+    console.log('King of ' + suit);
 }
 
 // Function
